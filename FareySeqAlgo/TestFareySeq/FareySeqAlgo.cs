66098e6cdc54b5361f40269486fdd3e86c2acef9
rjoshi <rjoshi@estee.com>
2018-02-01 22:56:17 -0800

    Refactoring for variable rename

--- FareySeqAlgo/TestFareySeq/FareySeqAlgo.cs
+++ FareySeqAlgo/TestFareySeq/FareySeqAlgo.cs
@@ -7,27 +7,27 @@ namespace TestFareySeq
 {
     class FareySeqAlgo
     {
-        public string getFareySeqForNumber(int p)
+        public string getFareySeqForNumber(int orderNum)
         {
             string result = "";
             double tempX = 0, tempY = 0;
             try
             {
                 result = "{";
-                if (p < 0)
+                if (orderNum < 0)
                 {
                     throw new FormatException("Number has to be greater or equal to 0");
                 }
                 // We know first two terms are 0/1 and 1/n
-                double x1 = 0, y1 = 1, x2 = 1, y2 = p;
+                double x1 = 0, y1 = 1, x2 = 1, y2 = orderNum;
                 result = result + x1 + "/" + y1;
                 result = result+", " + x2 + "/" + y2;
 
                 while (tempY != 1.0)
                 {
                     // Using recurrence relation to find the next term
-                    tempX = Math.Floor((y1 + p) / y2) * x2 - x1;
-                    tempY = Math.Floor((y1 + p) / y2) * y2 - y1;
+                    tempX = Math.Floor((y1 + orderNum) / y2) * x2 - x1;
+                    tempY = Math.Floor((y1 + orderNum) / y2) * y2 - y1;
                     //Capture result
                     result = result + ", " + tempX + "/" + tempY;
                     // Update x1, y1, x2 and y2 for next iteration
@@ -43,7 +43,7 @@ namespace TestFareySeq
             {
 
                 throw;
-            }
+            }   
             return result;
         }
     }
